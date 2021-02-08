ff422179bd8aee4456c473d73bab988aa81d153e
yolanda raventos <admin1gorotrade3000.com>
2021-02-08 16:28:39 +0100

    Tasca 2

--- index.php
+++ index.php
@@ -11,20 +11,28 @@ and open the template in the editor.
         <meta name="description"content="Primera pagina web">
         <meta name="viewport" content="2width=devide-width, inital-scale=1.0"> 
         <link rel="stylesheet" href="./css/estilos.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"><!-- comment -->
         <title>GORO TRADE 3000</title>
-
-                     
+        
+                          
     </head>
     
     <body>
-        <header>
-            <h1>GORO TRADE 3000</h1>
+        
+        <header
+             <h1>GORO TRADE 3000</h1>
+             
+        <div class="social">
+             <i class="fab fa-facebook-f"></i>         
+             <i class="fab fa-instagram"></i>
+             <i class="fab fa-twitter"></i><!-- comment -->
+            
+        </div>  
             
           <div class="iconos">
          
             <img class="icono" src="imagenes/logo_goro.png" />
-            <img class="icono" src="imagenes/logo_goro.png" />
-            <img class="icono" src="imagenes/logo_goro.png" />
+
            </div>
             
                         
@@ -33,15 +41,15 @@ and open the template in the editor.
         
         <nav >
             <ul class="menu">
-                <li><a href="#">Menu1</a></li>
-                <li><a href="#">Menu2</a></li>
-                <li><a href="#">Menu3</a></li>
-                <li><a href="#">Menu4</a></li>
-                <li><a href="#">Menu4</a></li>
+                <li><a href="#">Precios</a></li>
+                <li><a href="#">Amazon</a></li>
+                <li><a href="#">Catalogo</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Nuestra historia</a></li>
             </ul>        
                         
         </nav>
-        <div class="grid-container">
+        <div class="grid-container" >
             <div class="item">Maquillage infantil</div>
             <div class="item">Maquillaje adulto</div>
             <div class="item">Crema cosmetica</div>
@@ -53,16 +61,19 @@ and open the template in the editor.
                 <h2>El mejor maquillage</h2>
                 <p>Seleccion y maquetación personalizada</p>
                 <img class="foto" src="imagenes/Adulto.jpg" />
+                <input type="button" value="ver detalles">
              </article>
              <article>
                 <h2>Con su mejor packaging</h2>
                 <p>Packaging totalmente en carton</p>
                 <img class="foto" src="imagenes/Packaging.jpg" />
+                <input type="button" value="ver detalles">
              </article>
              <article>
                 <h2>Ingredientes veganos</h2>
                 <p>Todos los ingredientes son naturales y veganos</p>
                 <img class="foto" src="imagenes/ingredientes.jpg" />
+                <input type="button" value="ver detalles">
              </article>
             
         </div>   
