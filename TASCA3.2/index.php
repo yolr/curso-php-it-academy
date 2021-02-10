801374b304c57b3a1dcd486d356c765f2805e022
yolanda raventos <admin1gorotrade3000.com>
2021-02-10 15:45:26 +0100

    TASCA 3

---
+++ TASCA3.2/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./estilos.css">
+        <title></title>
+    </head>
+    <body>
+        <div class="strips">
+  <div class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title">IT</h1>            
+    </div>
+  </div>
+  <div class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title">A</h1>      
+    </div>
+  </div>
+  <div class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title">CA</h1>        
+    </div>
+  </div>
+  <div class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title">DE</h1>      
+    </div>
+  </div>
+  <div class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title">MY ♥</h1>            
+    </div>
+  </div>
+  <i class="fa fa-close strip__close"></i>
+</div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
