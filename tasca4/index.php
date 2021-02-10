7cfe9303ae9c474b12adb6cecbb351a2b2e0ddfb
yolanda raventos <admin1gorotrade3000.com>
2021-02-10 18:31:28 +0100

    TASCA 4

---
+++ tasca4/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TASCA4</title>
+        <meta charset="utf-8"><!-- comment -->
+        <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie-edge"><!-- comment -->
+        <link rel="styleshee" type="text/css" href="./tasca4/css/bootstrap.css">
+        <style type="text/css">
+            .borde {
+                backgroud-color: gray;
+                height: 50px;
+            }
+            .borde1 {
+                backgroud-color: aquamarine;
+                height: 150px;
+            }
+            .borde2 {
+                backgroud-color: black;
+                height: 200px;
+            }
+        </style>
+    </head>
+    <body>
+        <p> Bienvenido </p>
+        <div class="container">
+            <div class="row">
+                <div class="col borde">  </div>
+                <div class="col borde1">  </div>
+                <div class="col borde2">  </div>
+            </div>
+        </div>
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>    
+    <script src="js/bootstrap.min.js"></script>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
