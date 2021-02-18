c935dbb995317550d0a33c34538f8b4e208e261a
yolanda raventos <admin1gorotrade3000.com>
2021-02-18 15:01:54 +0100

    tasca4

--- tasca4/index.php
+++ tasca4/index.php
@@ -11,31 +11,63 @@ and open the template in the editor.
         <meta charset="utf-8"><!-- comment -->
         <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie-edge"><!-- comment -->
-        <link rel="styleshee" type="text/css" href="./tasca4/css/bootstrap.css">
-        <style type="text/css">
-            .borde {
-                backgroud-color: gray;
-                height: 50px;
-            }
-            .borde1 {
-                backgroud-color: aquamarine;
-                height: 150px;
-            }
-            .borde2 {
-                backgroud-color: black;
-                height: 200px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
+        <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+        <link rel="stylesheet" href="styles.css"/>
     </head>
     <body>
-        <p> Bienvenido </p>
-        <div class="container">
-            <div class="row">
-                <div class="col borde">  </div>
-                <div class="col borde1">  </div>
-                <div class="col borde2">  </div>
-            </div>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <img src="img/logo_goro.jpg" width="width" height="height" alt="alt"/>
+            <button class="nav-item active">Login</button>
+
+            <button class="nav-item">Registrarse</button>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <ion-icon name="menu-outline"></ion-icon>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Login<span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Registrarse</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
+      </li>
+
+    </ul>
+
+  </div>
+</nav>
+
+
+<!-- Page Content -->
+<section id="fondo">
+<div class="container">
+  <h1 class="mt-4">GORO TRADE 3000</h1>
+  <p>El mejor maquillage infantil vegano, para jugar  y disfrutar</p>
+</div>
+</section>
+
+    <div scr="img/caja.jpg"</div>
+    <div scr="img/caja.jpg"</div><!-- comment -->
+    <div scr="img/caja.jpg"</div><!-- comment -->
+
+
+
         
    
         
@@ -50,7 +82,8 @@ and open the template in the editor.
         
         
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="js/bootstrap.min.js"></script>
         <?php
         // put your code here
