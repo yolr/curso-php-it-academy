9a376750f54d528d1a037ef5d915b3c67a1da2a0
yolanda raventos <admin1gorotrade3000.com>
2021-02-24 17:50:55 +0100

    tasca 4  version definitiva , que sea lo que Dios quiera

--- tasca4/index.php
+++ tasca4/index.php
@@ -8,118 +8,231 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>TASCA4</title>
-        <meta charset="utf-8"><!-- comment -->
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie-edge"><!-- comment -->
-        <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+        <meta http-equiv="x-ua-compatible" content="ie-edge">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
         <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+        <!-- <link rel="stylesheet" type="text/css" href="./css/bootstrap.css"> -->
+        
+        <!--- START BOOTSTRAP CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!--- END BOOTSTRAP CDN -->
+        
         <link rel="stylesheet" href="styles.css"/>
     </head>
     <body>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <img src="img/logo_goro.jpg" width="width" height="height" alt="alt"/>
+
+            
+                  <!-- Page Content -->
+         <section id="fondo">
+                  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <img src="img/logo_goro.jpg" width="width" height="height" alt="alt"/>   
+             
+             
+  
+            
+            
+            
+            
+            
             <!-- Trigger the modal with a button -->
             <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Log in</button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
+            
+            
+            
+                        <!-- Trigger the modal with a button -->
+            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModa2">Registrarse</button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
+            
+            
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+        </nav>
 
 
+
+
+  
+      <!-- Modal -->
+      <div id="myModal" class="modal fade" role="dialog" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <!-- Modal content-->
+          
+          
+
+     
+          
+                    <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
               
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <ion-icon name="menu-outline"></ion-icon>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Login<span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Registrarse</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-
-    </ul>
-
+                         <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-</nav>
-
-
-<!-- Page Content -->
-<section id="fondo">
-<div class="container">
-  <h1 class="mt-4">GORO TRADE 3000</h1>
-  <p>El mejor maquillage infantil vegano, para jugar  y disfrutar</p>
-</div>
-</section>
-<section ud="portfolio"
-         <div class="container-fluid"
-           <div class="container-center"
-           <h1>ENVASES</h1>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form> 
+                
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
         </div>
-        
+      </div>
 
-        <img src="img/caja.jpg" width="500" height="1000"alt="alt"/>
-        <img src="img/doypack.jpg" width="500" height="1000"alt="alt"/><!-- comment -->
-        <img src="img/sachet.jpg" width="500" height="1000"alt="alt"/><!-- comment -->
-        <img src="img/tubo (1).jpg" width="500" height="1000"alt="alt"/>
-
-    
-</section>
-
-<!-- comment -->
+                   
+          
+             
+         
+      
+      
 
 
 
-        
-   
-        
-        
-        
-        
-        
-        
-        
-             <!-- Modal -->
-            <div id="myModal" class="modal fade" role="dialog" tabindex="-1" role="dialog>
-              <div class="modal-dialog">
+     
 
-                <!-- Modal content-->
+      <!-- Moda2 -->
+      <div id="myModa2" class="modal fade" role="dialog" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <!-- Modal content-->
+          
+          
+                    <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Registrarse</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            
+                         <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
                 
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Log in </h4>
-                  </div>
-                  <div class="modal-body">
-                    <p>Introduzca numero de usuario</p>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                  </div>
-                
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
-              </div>
-            </div>       
-        
-        
-        
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script> -->
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" > --> 
-    <script src="js/bootstrap.min.js"></script>
-        <?php
-        // put your code here
-        ?>
+
+          
+              
+ 
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>         
+          
+          
+          
+          
+ 
+       
+      
+      
+      
+                 
+        <div class="container">
+            <h1 class="mt-4">GORO TRADE 3000</h1>
+            <p>El mejor maquillage infantil vegano, para jugar  y disfrutar</p>
+        </div>
+         </section>   
+              
+      
+      
+      
+      
+      
+      
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="img/caja.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="img/doypack.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="img/sachet.jpg" alt="Third slide">
+    </div>
+  </div>
+</div>
+
+      <!--- START BOOTSTRAP CDN -->
+      <script src="js/jquery.js"></script>
+      <!--script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <!-- END BOOTTSTRAP CDN -->     
+     
+      <!-- <script src="js/bootstrap.min.js"></script> -->
+      
     </body>
 </html>
