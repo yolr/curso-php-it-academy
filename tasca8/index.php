8a9f8bab00e904b9972fa7747a25e48977772ec0
yolanda raventos <admin1gorotrade3000.com>
2021-03-08 11:16:47 +0100

    tasca 8

---
+++ tasca8/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$servername = "localhost";
+$database = "compra";
+$username = "username";
+$password = "password";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
