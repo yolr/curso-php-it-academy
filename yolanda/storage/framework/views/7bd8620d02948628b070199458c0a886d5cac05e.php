175d74c9390a7a16bab781e479b232da59584062
yolanda raventos <admin1gorotrade3000.com>
2021-05-17 17:58:01 +0200

    tasca8

---
+++ yolanda/storage/framework/views/7bd8620d02948628b070199458c0a886d5cac05e.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PROYECTOS</title>
+    </head>
+    <body>
+        <h1>PROYECTOS</h1>
+        <?php
+        
+        ?>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\proyectoweb\yolanda\resources\views/proyectos.blade.php ENDPATH**/ ?>
